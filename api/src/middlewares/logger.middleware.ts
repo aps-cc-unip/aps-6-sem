4d6b1b40afcbed08c9bb751eeb2b5663bd0d0164
Eder Lima <lima.eder101@gmail.com>
2022-11-05 01:25:58 -0300

    [api] feat: update ip strategy and add request perf

--- api/src/middlewares/logger.middleware.ts
+++ api/src/middlewares/logger.middleware.ts
@@ -6,13 +6,17 @@ export class LoggerMiddleware implements NestMiddleware {
   constructor(private readonly logger: Logger) {}
 
   use(req: Request, res: Response, next: NextFunction) {
+    const now = Date.now()
     const context = 'LoggerMiddleware'
-    const message = `${req.method} ${req.url} ${req.socket.remoteAddress}`
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    const message = `${req.method} ${req.url} ${ip}`
 
     this.logger.log(message, context)
 
     req.on('end', () => {
-      const message = `${req.method} ${req.url} ${req.socket.remoteAddress} ${res.statusCode}`
+      const end = Date.now()
+      const time = end - now
+      const message = `${req.method} ${req.url} ${ip} ${res.statusCode} took ${time}ms`
 
       if (res.statusCode >= 400) {
         return this.logger.error(message, context)
